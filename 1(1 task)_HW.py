1a6b65463dcf8653930766f97df023836ce89669
Stanislav Horodnychyi <stas.horodnychyy@gmail.com>
2019-08-11 15:16:34 +0300

    add 1 HW (tasks 1 and 2)

---
+++ 1(1 task)_HW.py
@@ -0,0 +1,7 @@
+a = int(input("write 1 number: "))
+b = int(input("write 2 number: "))
+
+print("result of a+b is: " + str(a+b) + "\n"
+     + "result of a-b is: " + str(a-b) + "\n"
+     + "result of a*b is: " + str(a*b) +"\n"
+     + "result of a/b is: " + str(a/b))
---
+++ 1(2 task)_HW.py
@@ -0,0 +1,5 @@
+name = input("What is your name? \n")
+age = input("How old are ypu? \n")
+adress = input("Where do you live? \n")
+print("Hello, "+ name +". Your age is " + age+
+     ". You live in " + adress)

60948313d3d2decdacbe71a4a1c79e2822617cdf
Stanislav Horodnychyi <stas.horodnychyy@gmail.com>
2019-08-12 08:30:58 +0300

    cosmetic change in 5(7)_HW

--- 5(7 task)_HW.py
+++ 5(7 task)_HW.py
@@ -2,10 +2,10 @@ name_list = ("Lubov", "Natalia", "Vira", "Nadia")
 name_list = list(name_list)
 parse_names = []
 changer = []
-for i in range(4):
-    for j in range(len(name_list)):
-        parse_names = list(name_list[j])
-        print(" ")
-        for q in range(len(parse_names)):
-            changer = list(parse_names[q]+"#")
-            print(changer)
+
+for j in range(len(name_list)):
+    parse_names = list(name_list[j])
+    print(" ")
+    for q in range(len(parse_names)):
+        changer = list(parse_names[q])
+        print(changer, end="#")

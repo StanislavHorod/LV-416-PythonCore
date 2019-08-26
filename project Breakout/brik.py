d7dce34032d960ab58d1d6a7c3848b6dc06710e6
Stanislav Horodnychyi <stas.horodnychyy@gmail.com>
2019-08-26 21:39:26 +0300

    Creat project files: colosrs, game_objects, brik, and ball

---
+++ project Breakout/brik.py
@@ -0,0 +1,32 @@
+import pygame
+
+from game_object import GameObject
+
+
+class Brick(GameObject):
+    def __init__(self, x, y, w, h, color, special_effect=None):
+        GameObject.__init__(self, x, y, w, h)
+        self.color = color
+        self.special_effect = special_effect
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.bounds)
+
+# class Triangle_Brick(GameObject):
+#     def __init__(self, x, y, w, h, color, special_effect=None):
+#         GameObject.__init__(self, x, y, w, h)
+#         self.color = color
+#         self.special_effect = special_effect
+
+#     def draw(self, surface):
+#         pygame.draw.triangle(surface, self.color, self.bounds)
+
+# class Geksa_Brick(GameObject):
+#     def __init__(self, x, y, w, h, color, special_effect=None):
+#         GameObject.__init__(self, x, y, w, h)
+#         self.color = color
+#         self.special_effect = special_effect
+#
+#     def draw(self, surface):
+#         pygame.draw.gecta(surface, self.color, self.bounds)
+#need moar info about construct gecta and triangle brigs
